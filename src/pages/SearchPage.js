d75002a76417bb94ab4ed220e9aae6062013737b
Bobby Keel <bobbykeel76@gmail.com>
2021-09-21 21:55:32 -0500

    search page with search api results

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,18 +1,87 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Search from '../components/SearchComponent';
 import { useStateValue } from '../redux/StateProvider';
 import '../styles/SearchPageStyles.css';
+import useGoogleSearch from '../useGoogleSearch';
+import SearchIcon from '@mui/icons-material/Search';
+import DescriptionIcon from '@mui/icons-material/Description';
+import ImageIcon from '@mui/icons-material/Image';
+import LocalOfferIcon from '@mui/icons-material/LocalOffer';
+import RoomIcon from '@mui/icons-material/Room';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { ListItemSecondaryAction } from '@mui/material';
 
 function SearchPage() {
     const [{ term }, dispatch] = useStateValue();
+    // Live call
+    // const { data } = useGoogleSearch(term);
 
+    // console.log(data)
     return (
         <div className="searchPage">
             <div className="searchPage__header">
-                <h1>{term}</h1>
-            </div>
-            <div className="searchPage__results">
+                <Link to="/">
+                    <img className="searchPage__logo" src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt="" />
+                </Link>
+                <div className="searchPage__headerBody">
+                    <Search hideButtons />
 
+                    <div className="searchPage__options">
+                        <div className="searchPage__optionsLeft">
+                            <div className="searchPage__option">
+                                <SearchIcon />
+                                <Link to="/all">All</Link>
+                            </div>
+                            <div className="searchPage__option">
+                                <DescriptionIcon />
+                                <Link to="/news">News</Link>
+                            </div>
+                            <div className="searchPage__option">
+                                <ImageIcon />
+                                <Link to="/images">Images</Link>
+                            </div>
+                            <div className="searchPage__option">
+                                <LocalOfferIcon />
+                                <Link to="/shopping">Shopping</Link>
+                            </div>
+                            <div className="searchPage__option">
+                                <RoomIcon />
+                                <Link to="/maps">Maps</Link>
+                            </div>
+                            <div className="searchPage__option">
+                                <MoreVertIcon />
+                                <Link to="/more">More</Link>
+                            </div>
+                        </div>
+                        <div className="searchPage__optionsRight">
+                            <div className="searchPage__option">
+                                <Link to="/settings">Settings</Link>
+                            </div>
+                            <div className="searchPage__option">
+                                <Link to="/tools">Tools</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+            {true && (
+                <div className="searchPage__results">
+                    <p className="searchPage__resultCount">
+                        About {data?.searchInformation.formattedTotalResults} results ({data?.searchInformation.formattedSearchTime} seconds for {term})
+                    </p>
+                    {data?.items.map(item => (
+                        <div className="searchPage__result">
+                            <a href={item.link}>
+                                {item.displayLink}
+                            </a>
+                            <a className="searchPage__Title" href={item.link}>
+                                <h1>{item.title}</h1>
+                            </a>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     )
 }
---
+++ src/response.js
@@ -0,0 +1,21 @@
+{
+    kind: 'customsearch#search', url: {…}, queries: {…}, context: {…}, searchInformation: {…}, …}
+    context: {title: 'Google'}
+    items: Array(10)
+    0: {kind: 'customsearch#result', title: 'Tesla: Electric Cars, Solar & Clean Energy', htmlTitle: '<b>Tesla</b>: Electric Cars, Solar &amp; Clean Energy', link: 'https://www.tesla.com/', displayLink: 'www.tesla.com', …}
+    1: {kind: 'customsearch#result', title: 'Tesla Investor Relations', htmlTitle: '<b>Tesla</b> Investor Relations', link: 'https://ir.tesla.com/', displayLink: 'ir.tesla.com', …}
+    2: {kind: 'customsearch#result', title: 'Model S | Tesla', htmlTitle: 'Model S | <b>Tesla</b>', link: 'https://www.tesla.com/models', displayLink: 'www.tesla.com', …}
+    3: {kind: 'customsearch#result', title: 'Tesla - Keep it Real Tour', htmlTitle: '<b>Tesla</b> - Keep it Real Tour', link: 'https://teslatheband.com/', displayLink: 'teslatheband.com', …}
+    4: {kind: 'customsearch#result', title: 'Model 3 | Tesla', htmlTitle: 'Model 3 | <b>Tesla</b>', link: 'https://www.tesla.com/model3', displayLink: 'www.tesla.com', …}
+    5: {kind: 'customsearch#result', title: 'Q2 2021 Update', htmlTitle: 'Q2 2021 Update', link: 'https://tesla-cdn.thron.com/static/ZBOUYO_TSLA_Q2_…disposition=inline%3Bfilename%3D%22q2_2021.pdf%22', displayLink: 'tesla-cdn.thron.com', …}
+    6: {kind: 'customsearch#result', title: 'Model X | Tesla', htmlTitle: 'Model X | <b>Tesla</b>', link: 'https://www.tesla.com/modelx', displayLink: 'www.tesla.com', …}
+    7: {kind: 'customsearch#result', title: 'Tesla, Inc. (TSLA) Stock Price, News, Quote & History - Yahoo Finance', htmlTitle: '<b>Tesla</b>, Inc. (TSLA) Stock Price, News, Quote &amp; History - Yahoo Finance', link: 'https://finance.yahoo.com/quote/TSLA/', displayLink: 'finance.yahoo.com', …}
+    8: {kind: 'customsearch#result', title: 'Tesla (@Tesla) | Twitter', htmlTitle: '<b>Tesla</b> (@<b>Tesla</b>) | Twitter', link: 'https://twitter.com/tesla', displayLink: 'twitter.com', …}
+    9: {kind: 'customsearch#result', title: 'Tesla Inc (TSLA : NASDAQ) Stock Price & News - Google Finance', htmlTitle: '<b>Tesla</b> Inc (TSLA : NASDAQ) Stock Price &amp; News - Google Finance', link: 'https://www.google.com/finance/quote/TSLA:NASDAQ', displayLink: 'www.google.com', …}
+    length: 10
+    [[Prototype]]: Array(0)
+    kind: "customsearch#search"
+    queries: {request: Array(1), nextPage: Array(1)}
+    searchInformation: {searchTime: 0.379904, formattedSearchTime: '0.38', totalResults: '310000000', formattedTotalResults: '310,000,000'}
+    url: {type: 'application/json', template: 'https://www.googleapis.com/customsearch/v1?q={sear…e?}&imgDominantColor={imgDominantColor?}&alt=json'}
+    [[Prototype]]: Object
